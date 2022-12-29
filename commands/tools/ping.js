a1e0e028590c258fb414e089ae97258e37095760
Crypto Gap <30596626+cryptogap@users.noreply.github.com>
2022-12-29 13:15:15 -0600

    updated ping to be an embed
    This update converted the original message reply to an embed reply.
    
    (Message reply code still in file, just commented out)

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,14 +9,55 @@ module.exports = {
 			fetchReply: true,
 		});
 
-		const newMessage = `API Latency is ${Math.round(
-			client.ws.ping
-		)}ms\n Message Latency is ${
-			message.createdTimestamp - interaction.createdTimestamp
-		}ms`;
+		//Deployes an embed instead of a message.
+		const pingEmbed = new EmbedBuilder()
+			.setColor(0x00ff00)
+			.setTitle("Ping Information")
+			.setURL("https://github.com/cryptogap/Axi")
+			.setAuthor({
+				name: "Axi Bot",
+				iconUrl:
+					"https://raw.githubusercontent.com/cryptogap/Axi/main/logo/bot_logo.png",
+			})
+			.setDescription(
+				"This command will show you the latency of the bot and the API."
+			)
+			.setThumbnail(
+				"https://raw.githubusercontent.com/cryptogap/Axi/main/logo/bot_logo.png"
+			)
+			.addFields([
+				{
+					name: "API Latency",
+					value: `${Math.round(client.ws.ping)}ms`,
+					inline: true,
+				},
+				{
+					name: "Message Latency",
+					value: `${message.createdTimestamp - interaction.createdTimestamp}ms`,
+					inline: true,
+				},
+			])
+			.setTimestamp()
+			.setFooter({
+				text: "Ping is the time it takes for a message to be sent and received.",
+				iconUrl:
+					"https://raw.githubusercontent.com/cryptogap/Axi/main/logo/bot_logo.png",
+			});
+
+		//Below is a standard reply, but we can use an embed instead.
+
+		// const newMessage = `API Latency is ${Math.round(
+		// 	client.ws.ping
+		// )}ms\n Message Latency is ${
+		// 	message.createdTimestamp - interaction.createdTimestamp
+		// }ms`;
+
+		// await interaction.editReply({
+		// 	content: newMessage,
+		// });
 
 		await interaction.editReply({
-			content: newMessage,
+			embeds: [pingEmbed],
 		});
 	},
 };
